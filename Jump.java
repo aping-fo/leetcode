8189711b4fb65f3137f6d1756dae4a9ec53863b2
aping.fo <125455364@qq.com>
2019-02-28 08:34:04 +0800

    Update Jump.java

--- Jump.java
+++ Jump.java
@@ -11,7 +11,8 @@
 
 public class Jump {
     static final int s = 100; //自定义的台阶数
-
+    
+    /**2种步做法*/
     static int compute(int stair){
         if ( stair <= 0){
             return 0;
@@ -25,6 +26,29 @@ public class Jump {
         return compute(stair-1) + compute(stair-2);
     }
 
+    
+    public class Jump {
+    static final int s = 100; //自定义的台阶数
+    
+    /**3种步，1,2,3台阶做法*/
+    static int compute(int stair){
+        if ( stair <= 0){
+            return 0;
+        }
+        if (stair == 1){
+            return 1;
+        }
+        if (stair == 2){
+            return 2;
+        }
+        
+        // 111 21 12 3
+        if (stair == 3){
+            return 4;
+        }
+        return compute(stair-1) + compute(stair-2) + compute(star -3);
+    }
+    
     public static void main(String args[]) {
         System.out.println("共有" + compute(s) + "种走法");
     }
