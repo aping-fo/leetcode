2855d3c6615da944e392bd7151c2aba21689fe20
aping.fo <125455364@qq.com>
2019-03-01 09:29:00 +0800

    Create LongestValidParentheses.java

---
+++ LongestValidParentheses.java
@@ -0,0 +1,33 @@
+package com.ap;
+
+public class LongestValidParentheses {
+
+    private static final char left = '(';
+    private static final char right = ')';
+
+    public static void main(String[] args) {
+        System.out.println(longestValidParentheses("(()()("));
+    }
+
+    public static int longestValidParentheses(String s) {
+        char[] chars = s.toCharArray();
+        return Math.max(calc(chars, 0, 1, chars.length, '('), calc(chars, chars.length - 1, -1, -1, ')'));
+    }
+
+    private static int calc(char[] chars, int i, int flag, int end, char cTem) {
+        int max = 0, sum = 0, currLen = 0, validLen = 0;
+        for (; i != end; i += flag) {
+            sum += (chars[i] == cTem ? 1 : -1);
+            currLen++;
+            if (sum < 0) {
+                max = max > validLen ? max : validLen;
+                sum = 0;
+                currLen = 0;
+                validLen = 0;
+            } else if (sum == 0) {
+                validLen = currLen;
+            }
+        }
+        return max > validLen ? max : validLen;
+    }
+}
